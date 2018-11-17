09fc526541914a8837762c86588034d613955b81
asharijuang <asharijuang@me.com>
2018-11-17 14:07:22 +0700

    Initial Commit

---
+++ QTYButtons/QTYButtons.h
@@ -0,0 +1,19 @@
+//
+//  QTYButtons.h
+//  QTYButtons
+//
+//  Created by Qiscus on 17/11/18.
+//  Copyright © 2018 Qiscus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QTYButtons.
+FOUNDATION_EXPORT double QTYButtonsVersionNumber;
+
+//! Project version string for QTYButtons.
+FOUNDATION_EXPORT const unsigned char QTYButtonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QTYButtons/PublicHeader.h>
+
+
